d8d2c3e3659f448456b375e283aa3fa27325e666
badri <badri@kayadev.com>
2019-09-26 12:20:44 +0530

    added dataset for training

--- examples/custom_detection_train.py
+++ examples/custom_detection_train.py
@@ -2,8 +2,8 @@ from imageai.Detection.Custom import DetectionModelTrainer
 
 trainer = DetectionModelTrainer()
 trainer.setModelTypeAsYOLOv3()
-trainer.setDataDirectory(data_directory="hololens")
-trainer.setTrainConfig(object_names_array=["hololens"], batch_size=4, num_experiments=200, train_from_pretrained_model="pretrained-yolov3.h5")
+trainer.setDataDirectory(data_directory="drone")
+trainer.setTrainConfig(object_names_array=["person", "light_motor_vehicle", "heavy_motor_vehicle"], batch_size=4, num_experiments=200, train_from_pretrained_model="pretrained-yolov3.h5")
 #download pre-trained model via https://github.com/OlafenwaMoses/ImageAI/releases/download/essential-v4/pretrained-yolov3.h5
 # If you are training to detect more than 1 object, set names of objects above like object_names_array=["hololens", "google-glass", "oculus", "magic-leap"]
 trainer.trainModel()
